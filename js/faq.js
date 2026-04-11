e0672c56239cbbac15889657e7f897d7ca72e68a
xsabduuh <abdosiifoou@gmail.com>
2026-04-11 12:53:25 +0100

    Delete js directory

--- js/faq.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.faq-question').forEach(question => {
-  question.addEventListener('click', () => {
-    const faqItem = question.parentElement;
-    faqItem.classList.toggle('active');
-  });
-});
